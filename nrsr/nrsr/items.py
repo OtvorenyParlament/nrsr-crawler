2c456acd5aceab90a5335f4be80fc575526d1666
Jozef Sukovsky <sukovskyj@gmail.com>
2018-10-04 22:02:48 +0200

    members spider

--- nrsr/nrsr/items.py
+++ nrsr/nrsr/items.py
@@ -3,7 +3,15 @@ NRSR Items
 """
 
 import scrapy
-from scrapy.loader.processors import TakeFirst
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+
+
+def filter_whitespaces(value):
+    return value.replace('\xa0', '').strip()
+
+
+def filter_mailto(value):
+    return value.replace('mailto:', '')    
 
 
 class ClubItem(scrapy.Item):
@@ -19,3 +27,44 @@ class ClubItem(scrapy.Item):
 class ClubMemberItem(scrapy.Item):
     external_id = scrapy.Field()
     membership = scrapy.Field()
+
+
+class MemberItem(scrapy.Item):
+    type = scrapy.Field(output_processor=TakeFirst())
+    external_id = scrapy.Field(output_processor=TakeFirst())
+    forename = scrapy.Field(output_processor=TakeFirst())
+    surname = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(
+        input_processor=MapCompose(filter_whitespaces),
+        output_processor=Join()
+    )
+    stood_for_party = scrapy.Field(
+        input_processor=MapCompose(filter_whitespaces),
+        output_processor=TakeFirst()
+    )
+    born = scrapy.Field(
+        input_processor=MapCompose(filter_whitespaces),
+        output_processor=Join()
+    )
+    nationality = scrapy.Field(
+        input_processor=MapCompose(filter_whitespaces),
+        output_processor=Join()
+    )
+    residence = scrapy.Field(
+        input_processor=MapCompose(filter_whitespaces),
+        output_processor=Join()
+    )
+    county = scrapy.Field(
+        input_processor=MapCompose(filter_whitespaces),
+        output_processor=Join()
+    )
+    email = scrapy.Field(
+        input_processor=MapCompose(filter_mailto),
+        output_processor=Join()
+    )
+    images = scrapy.Field()
+    image_urls = scrapy.Field()
+    period_num = scrapy.Field(output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
+
+    memberships = scrapy.Field()
---
+++ nrsr/nrsr/spiders/members.py
@@ -0,0 +1,112 @@
+"""
+Members
+"""
+
+from urllib.parse import urlparse, parse_qs
+import scrapy
+
+from nrsr.items import MemberItem
+
+class MembersSpider(scrapy.Spider):
+    name = 'members'
+    BASE_URL = 'https://www.nrsr.sk/web/'
+
+    def start_requests(self):
+        urls = [
+            'https://www.nrsr.sk/web/?SectionId=60',
+
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        periods = response.xpath('//*[@id="_sectionLayoutContainer_ctl01__currentTerm"]/option/@value').extract()
+        i = 0
+        for period in periods:
+            if i == 0:
+                links = response.xpath(
+                    '//*[@id="_sectionLayoutContainer__panelContent"]/div/div/ul/li/a/@href').extract()
+                for link in links:
+                    yield scrapy.Request('%s%s' % (self.BASE_URL, link), callback=self.parse_member)
+            i += 1
+            viewstate = response.css('input#__VIEWSTATE::attr(value)').extract_first()
+            eventvalidation = response.css('input#__EVENTVALIDATION::attr(value)').extract_first()
+            viewstategenerator = response.css('input#__VIEWSTATEGENERATOR::attr(value)').extract_first()
+            yield scrapy.FormRequest(
+                response.request.url,
+                formdata={
+                    '__EVENTTARGET': '_sectionLayoutContainer$ctl01$_currentTerm',
+                    '_sectionLayoutContainer$ctl01$_currentTerm': str(period),
+                    '_sectionLayoutContainer$ctl01$ctlListType': 'abc',
+                    '__VIEWSTATE': viewstate,
+                    '__EVENTVALIDATION': eventvalidation,
+                    '__VIEWSTATEGENERATOR': viewstategenerator,
+                },
+                callback=self.parse_list
+            )
+    
+    def parse_list(self, response):
+        links = response.xpath(
+            '//*[@id="_sectionLayoutContainer__panelContent"]/div/div/ul/li/a/@href').extract()
+        for link in links:
+            yield scrapy.Request('%s%s' % (self.BASE_URL, link), callback=self.parse_member)
+
+
+    def parse_member(self, response):
+        item = scrapy.loader.ItemLoader(item=MemberItem())
+        url_parsed = urlparse(response.url)
+
+        panel_content = '[@id="_sectionLayoutContainer__panelContent"]'
+
+        item.add_value('type', 'member')
+        item.add_value('external_id', parse_qs(url_parsed.query)['PoslanecID'][0])
+        try:
+            item.add_value('period_num', parse_qs(url_parsed.query)['CisObdobia'][0])
+        except:
+            pass
+        item.add_value('url', response.url)
+        item.add_value('forename', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[1]/span/text()'.format(panel_content)
+        ).extract_first())
+
+        item.add_value('surname', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[3]/span/text()'.format(panel_content)
+        ).extract_first())
+        title = response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[2]/span/text()'.format(panel_content)
+        ).extract_first()
+        if not title:
+            title = None
+        item.add_value('title', title)
+        item.add_value('stood_for_party', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[4]/span/text()'.format(panel_content)
+        ).extract_first())
+
+        item.add_value('born', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[5]/span/text()'.format(panel_content)
+        ).extract_first())
+
+        item.add_value('nationality', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[6]/span/text()'.format(panel_content)
+        ).extract_first())
+
+        item.add_value('residence', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[7]/span/text()'.format(panel_content)
+        ).extract_first())
+
+        item.add_value('county', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[8]/span/text()'.format(panel_content)
+        ).extract_first())
+
+        item.add_value('email', response.xpath(
+            '//*{}/div[1]/div[1]/div[1]/div[9]/span/a/@href'.format(panel_content)
+        ).extract_first())
+
+        item.add_value('image_urls', response.xpath(
+            '//*{}/div[1]/div[2]/div/img/@src'.format(panel_content)).extract())
+
+        # memberships
+        memberships = response.xpath(
+            '//*{}/div[1]/div[1]/div[2]/ul/li/text()'.format(panel_content)).extract()
+        item.add_value('memberships', memberships)
+        yield item.load_item()
