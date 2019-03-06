43fe3c99b87ad94451d60a6b2b7125778a97372d
Jozef Sukovsky <sukovskyj@gmail.com>
2019-03-06 23:06:50 +0100

    increased delay, decreased history crawling

--- nrsr/settings.py
+++ nrsr/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = .6
+DOWNLOAD_DELAY = 2.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 CONCURRENT_REQUESTS_PER_IP = 1
@@ -120,4 +120,4 @@ MONGO_DATABASE = 'nrsr'
 MONGO_URI = 'mongodb://localhost'
 MONGO_COL = 'crawled_data'
 MONGO_COL_ARCHIVE = 'archived_data'
-DAILY_HISTORY_DAYS = 30
+DAILY_HISTORY_DAYS = 7
